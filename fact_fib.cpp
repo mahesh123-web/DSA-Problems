56b73ca6854523f359bdabb2d6637be8e3af1099
maheshdhanve <maheshdhanve317@gmail.com>
2026-03-24 10:30:49 +0530

    Fibonacci and factorial

---
+++ fact_fib.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int n){
+    if(n == 0) return 1;
+    return n * factorial(n-1);
+}
+
+int fib(int n){
+    if(n <= 1) return n;
+    return fib(n-1) + fib(n-2);
+}
+
+int main(){
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    cout<<"The factorial of the number is: ";
+    cout<<factorial(n)<<endl;
+    
+    cout<<"The fibonacci series is : ";
+    for(int i = 0; i<n; i++){
+        cout<<fib(i)<<"  ";
+    }
+    
+    return 0;
+}
